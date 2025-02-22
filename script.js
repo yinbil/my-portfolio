9209381e8b1d02dfb444e705073fe894b6689aa7
yinbil <francis.yinbil@ashesi.edu.gh>
2025-02-22 12:45:12 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+window.onscroll = function() {
+    let button = document.getElementById("back-to-top");
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+        button.style.display = "block";
+    } else {
+        button.style.display = "none";
+    }
+};
+
+function scrollToTop() {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+}
